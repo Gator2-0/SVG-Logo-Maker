ebca5ffc92b7ac484fdf5b99a12fa35c1227f8cf
Jeremy <jejedech.63@gmail.com>
2023-04-22 11:09:53 +1000

    addind basic prompt

--- server.js
+++ server.js
@@ -1,2 +1,29 @@
 const inquirer = require('inquirer');
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What text do you want in your logo (3 letters max)?',
+      name: 'text',
+    },
+    {
+      type: 'input',
+      message: 'What color do you want?',
+      name: 'color',
+    },
+    {
+      type: 'list',
+      message: 'What shape do you want to use?',
+      choices: ['triangle','circle','square'],
+      name: 'confirm',
+    },
+    {
+      type: 'input',
+      message: 'What color do you want the shape to be?',
+      name: 'shapeColor',
+    },
+  ])
+  .then((response) =>
+    console.log('finished')
+  );
