8e83d67aa8974e0678ea526fd87a60507834d7d7
Jeremy <jejedech.63@gmail.com>
2023-04-22 15:26:01 +1000

    create parent class

--- assets/shapes.js
+++ assets/shapes.js
@@ -1,54 +1,42 @@
 const fs = require('fs');
 
-class Square {
-  constructor(text, textcolor, shapeColor){
+class Shape {
+  constructor(text, textColor, shapeColor){
     this.text = text;
-    this.textColor = textcolor;
+    this.textColor = textColor;
     this.shapeColor = shapeColor;
+    this.shape;
   }
-
   create(){
     let xml = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-    <rect width="300" height="200" x="5" style="fill:${this.shapeColor};stroke-width:3;stroke:rgb(0,0,0)" />
+    <${this.shape} style="fill:${this.shapeColor};stroke-width:3;stroke:rgb(0,0,0)" />
     <text x="100" y="110" font-size="50" font-weight="bold" fill="${this.textColor}">${this.text}</text>
     </svg>`
+    console.log(`SVG created with info:\ntext:${this.text}\ntext color:${this.textColor}\nshape color:${this.shapeColor}`)
 
     fs.writeFile('logo.svg',xml, (error)=>error? console.log(error) : console.log('SVG successfully created!!'));
-
   }
-  
 }
+
+class Square extends Shape {
+  constructor(text, textColor, shapeColor){
+    super(text, textColor, shapeColor)
+    this.shape = 'rect width="300" height="200" x="5"';
+  }
+}
+
 class Circle{
-  constructor(text, textcolor, shapeColor){
-    this.text = text;
-    this.textColor = textcolor;
-    this.shapeColor = shapeColor;
+  constructor(text, textColor, shapeColor){
+    super(text, textColor, shapeColor)
+    this.shape = 'circle cx="100" cy="100" r="35%"';
   }
-
-  create(){
-    return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="100" cy="100" r="35%" style="fill:${this.shapeColor};stroke-width:3;stroke:rgb(0,0,0)" />
-    <text x="50" y="110" font-size="50" font-weight="bold" fill="${this.textColor}">${this.text}</text>
-  </svg>`
-  }
-
-
 }
 
 class Triangle{
-  constructor(text, textcolor, shapeColor){
-    this.text = text;
-    this.textColor = textcolor;
-    this.shapeColor = shapeColor;
+  constructor(text, textColor, shapeColor){
+    super(text, textColor, shapeColor)
+    this.shape = 'polygon points="150,10 250,180 50,180"';
   }
-
-  create(){
-    return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-    <polygon points="150,10 250,180 50,180" style="fill:${this.shapeColor};stroke-width:3;stroke:rgb(0,0,0)" />
-    <text x="100" y="150" font-size="50" font-weight="bold" fill="${this.textColor}">${this.text}</text>
-  </svg>`
-  }
-
 }
 
 module.exports = {Triangle, Circle, Square};
